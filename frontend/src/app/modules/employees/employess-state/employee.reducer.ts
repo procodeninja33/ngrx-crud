2fcea3a425dd71c7eba698178c7c01930fa88cc5
prakash <prakash.harvani>
2020-04-09 15:16:17 +0530

    issue resolved of listing

--- frontend/src/app/modules/employees/employess-state/employee.reducer.ts
+++ frontend/src/app/modules/employees/employess-state/employee.reducer.ts
@@ -1,32 +1,28 @@
 import * as employeeAction from './employee.actions';
 import * as fromRoot from './employee.state';
-import { employeeActionType } from './employee.actions';
+import * as employeeActionType from './employee.actions';
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { EntityState, EntityAdapter, createEntityAdapter } from "@ngrx/entity";
 import { Employees } from 'src/app/models/response.model';
 
 export interface EmployeeState extends EntityState<Employees> {
-    selectedCustomerId: number | null;
+    selectedEmployeeId: any | null;
     loading: boolean;
     loaded: boolean;
     error: string;
 }
 
-export interface AppState extends fromRoot.AppState{
+export interface AppState extends fromRoot.AppState {
     employees: EmployeeState;
 }
 
-export const employeeAdapter: EntityAdapter<Employees> = createEntityAdapter<Employees>({});
+export const employeeAdapter: EntityAdapter<Employees> = createEntityAdapter<Employees>();
 
-export const getEmployees = createSelector(
-    createFeatureSelector('employees'),
-    employeeAdapter.getSelectors().selectAll
-);
 
 export const defaultEmployee: EmployeeState = {
     ids: [],
     entities: {},
-    selectedCustomerId: null,
+    selectedEmployeeId: null,
     loading: false,
     loaded: false,
     error: "",
@@ -37,20 +33,25 @@ export const initialState = employeeAdapter.getInitialState(defaultEmployee);
 
 export function employeeReducer(state = initialState, action: employeeAction.action): EmployeeState {
     switch (action.type) {
-        case employeeActionType.LOAD_EMPLOYEE_SUCCESS:
-            return  employeeAdapter.addAll(
-                action['payload'], {
-                ...state,
-                loading: false,
-                loaded: true
-              });
-            // let data = {
-            //     ...state,
-            //     entities: action['payload']['data']['employees']
-            // }
-            // console.log('data', data);
-            // return data;
-        case employeeActionType.LOAD_EMOLOYEE_ERROR:
+
+        // case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS: {
+        //     return customerAdapter.addAll(action.payload, {
+        //         ...state,
+        //         loading: false,
+        //         loaded: true
+        //     });
+        // }
+
+        case employeeActionType.employeeActionType.LOAD_EMPLOYEE_SUCCESS:
+            {
+                return employeeAdapter.addAll(
+                    action['payload'], {
+                    ...state,
+                    loading: false,
+                    loaded: true
+                });
+            }
+        case employeeActionType.employeeActionType.LOAD_EMOLOYEE_ERROR:
             return {
                 ...state,
                 entities: {},
@@ -64,3 +65,13 @@ export function employeeReducer(state = initialState, action: employeeAction.act
 }
 
 
+
+
+const getEmployeeFeatureState = createFeatureSelector<EmployeeState>(
+    "employees"
+);
+
+export const getEmployees = createSelector(
+    getEmployeeFeatureState,
+    employeeAdapter.getSelectors().selectAll
+);
